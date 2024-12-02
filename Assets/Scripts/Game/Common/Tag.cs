0e77c07a7565e395e1bb660ce7989b5ed65c8ad2
mari <mariia999@bk.ru>
2024-12-03 02:22:36 +0300

    New scripts Tag

---
+++ Assets/Scripts/Game/Common/Tag.cs
@@ -0,0 +1,13 @@
+namespace Platformer.Game.Common
+{
+    public static class Tag
+    {
+        #region Variables
+
+        public const string Enemy = "Enemy";
+        public const string Player = "Player";
+        public const string PlayerBullet = "PlayerBullet";
+
+        #endregion
+    }
+}
