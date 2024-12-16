2ce889f647f23ca50dba18822b29850081ca8f18
mari <mariia999@bk.ru>
2024-12-16 16:40:51 +0300

    Add new scripts PickUps

---
+++ Assets/Scripts/Game/PickUps/HealthPack.cs
@@ -0,0 +1,29 @@
+using Platformer.Game.Common;
+using Platformer.Game.Player.Base;
+using UnityEngine;
+
+namespace Platformer.Game.PickUps
+{
+    public class HealthPack: PickUps
+    {
+        #region Variables
+
+        [Header(nameof(HealthPack))]
+        [SerializeField] private int _healthAmount = 20;
+
+        #endregion
+
+        #region Protected methods
+
+        protected override void PerformActions(Collider2D other)
+        {
+            base.PerformActions(other);
+            if (other.CompareTag(Tag.Player))
+            {
+                other.gameObject.GetComponent<PlayerDeath>().Heal(_healthAmount);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Scripts/Game/PickUps/PickUps.cs
@@ -0,0 +1,27 @@
+using Platformer.Game.Common;
+using UnityEngine;
+
+namespace Platformer.Game.PickUps
+{
+    public class PickUps: MonoBehaviour
+    {
+        #region Unity lifecycle
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag(Tag.Player))
+            {
+                PerformActions(other);
+                Destroy(gameObject);
+            }
+        }
+
+        #endregion
+
+        #region Protected methods
+
+        protected virtual void PerformActions(Collider2D other) { }
+
+        #endregion
+    }
+}
